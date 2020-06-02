21a588f51882239b2dc0ab8749f91d5681dc8018
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-06-02 17:48:19 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+# Made by Rajni
+cd
+pkg install fish git python python2 curl php figlet toilet -y
+clear
+
+figlet -f small Logo
+toilet --gay Rajni
+pip2 install --upgrade pip
+pip2 install  mechanize
+pip2 install requests
+clear
+figlet -f small Logo
+toilet --gay Rajni
+mkdir Rajni_Kant
+cd Rajni_Kant
+
+clear
+git clone https://github.com/mkdirlove/FBTOOL
+cd FBTOOL
+
+clear
+git clone https://github.com/V4N654T/dark-fb
+cd dark-fb
+cd ..
+clear 
+git clone https://github.com/thelinuxchoice/blackeye
+cd blackeye
