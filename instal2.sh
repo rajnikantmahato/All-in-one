2f840427b24487eba7212f6cd6d408c70fb68b4a
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-06-03 19:19:39 +0530

    Update instal2.sh

--- instal2.sh
+++ instal2.sh
@@ -4,3 +4,11 @@ cd
 
 cd  $PREFIX/etc/
 wget https://raw.githubusercontent.com/rajnikantmahato/All-in-one/master/bash.bashrc
+cd
+cd Rajni_Kant
+git clone https://github.com/thelinuxchoice/shellphish
+git clone https://github.com/techchipnet/CamPhish
+
+cd
+rm instal2.sh
+rm install.sh
